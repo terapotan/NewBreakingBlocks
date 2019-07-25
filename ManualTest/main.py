bb44515dcab0b2317a041eecd7abf71d3a9c0913
terapotan <rs582855@pc4.so-net.ne.jp>
2019-07-25 19:49:00 +0900

    60FPSで動作するように改良

--- ManualTest/main.py
+++ ManualTest/main.py
@@ -3,10 +3,8 @@ import unittest
 class mainTest(unittest.TestCase):
     def test_showBallOnTheDisplay(self):
         self.assertEqual(input("ボールが表示されたか(y/n)"),'y')
-    def test_checkBallKeyActionMove(self):
-        self.assertEqual(input("aキーを入力するとボールが右へ移動する(y/n)"),'y')
-    
-
+    def test_checkBallMoveEdgeOfScreen(self):
+        self.assertEqual(input('ボールが端から端まで移動したか(y/n)'),'y')
 
 # ほかのファイルからimportされたときにテストが実行されないようにする
 if __name__ == "__main__":
