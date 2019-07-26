29992fbced8ed6d9942491f79cfa2e735ccf64a7
terapotan <rs582855@pc4.so-net.ne.jp>
2019-07-26 20:02:40 +0900

    ボールの速度を変更出来るように修正

--- NewBreakingBlocks/NewBreakingBlocks/ball.cpp
+++ NewBreakingBlocks/NewBreakingBlocks/ball.cpp
@@ -6,6 +6,11 @@ ball::ball()
 	ballImageHandle = LoadGraph("../.././resource/ball.png");
 }
 
+ball::ball(int ballLeftupCoordX, int ballLeftupCoordY) : ball()
+{
+	this->setObjectCoordinate(ballLeftupCoordX, ballLeftupCoordY);
+}
+
 
 ball::~ball()
 {
@@ -15,6 +20,18 @@ ball::~ball()
 void ball::setObjectCoordinate(int x, int y) {
 	this->ballLeftupCoordX = x;
 	this->ballLeftupCoordY = y;
+}
+
+void ball::setBallVelocity(int ballVelocityX_PerFrame, int ballVelocityY_PerFrame)
+{
+	this->ballVelocityX_PerFrame = ballVelocityX_PerFrame;
+	this->ballVelocityY_PerFrame = ballVelocityY_PerFrame;
+}
+
+void ball::moveBallAtTheSetVelocity()
+{
+	this->ballLeftupCoordX += this->ballVelocityX_PerFrame;
+	this->ballLeftupCoordY += this->ballVelocityY_PerFrame;
 }
 
 
--- NewBreakingBlocks/NewBreakingBlocks/ball.h
+++ NewBreakingBlocks/NewBreakingBlocks/ball.h
@@ -1,15 +1,19 @@
 #pragma once
 #include "objectsOnTheScreen.h"
-
 class ball: public objectsOnTheScreen
 {
 private:
-	int ballLeftupCoordX;
-	int ballLeftupCoordY;
+	int ballLeftupCoordX;//‰æ–Ê‰E•ûŒü‚ğ³‚Æ‚·‚é
+	int ballLeftupCoordY;//‰æ–Ê‰º•ûŒü‚ğ³‚Æ‚·‚é
+	int ballVelocityX_PerFrame;//‰æ–Ê‰E•ûŒü‚ğ³‚Æ‚·‚é
+	int ballVelocityY_PerFrame;//‰æ–Ê‰º•ûŒü‚ğ³‚Æ‚·‚é
 	int ballImageHandle;
 public:
 	ball();
+	ball(int ballLeftupCoordX, int ballLeftupCoordY);
 	void setObjectCoordinate(int x, int y) override;
+	void setBallVelocity(int ballVelocityX_PerFrame, int ballVelocityY_PerFrame);
+	void moveBallAtTheSetVelocity();
 	void objectPaint() override;
 	virtual ~ball();
 };
