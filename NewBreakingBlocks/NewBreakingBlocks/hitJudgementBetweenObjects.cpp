9655b8881adc538e4d9ac035c19332a58c2dc1aa
terapotan <rs582855@pc4.so-net.ne.jp>
2019-08-06 14:32:45 +0900

    矩形同士の当たり判定仮実装

---
+++ NewBreakingBlocks/NewBreakingBlocks/hitJudgementBetweenObjects.cpp
@@ -0,0 +1,16 @@
+#include "hitJudgementBetweenObjects.h"
+
+
+bool hitJudgementBetweenObjects::hitJudgeRectAndRect(int sx_one, int sy_one, int width_one, int height_one, int sx_two, int sy_two, int width_two, int height_two)
+{
+	return true;
+}
+
+hitJudgementBetweenObjects::hitJudgementBetweenObjects()
+{
+}
+
+
+hitJudgementBetweenObjects::~hitJudgementBetweenObjects()
+{
+}
---
+++ NewBreakingBlocks/NewBreakingBlocks/hitJudgementBetweenObjects.h
@@ -0,0 +1,9 @@
+#pragma once
+class hitJudgementBetweenObjects
+{
+public:
+	bool hitJudgeRectAndRect(int sx_one, int sy_one, int width_one, int height_one, int sx_two, int sy_two, int width_two, int height_two);
+	hitJudgementBetweenObjects();
+	~hitJudgementBetweenObjects();
+};
+
